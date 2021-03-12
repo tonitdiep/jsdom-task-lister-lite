8b2d12c450996f51d490317a7cead01ff650ee76
tonitdiep <adiep123@gmail.com>
2021-03-12 01:06:47 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   //form and relevant input fields
   const newTaskForm = document.getElementById("create-task-form");
   const newTaskDescription = document.getElementById("new-task-description");
-  // const newTaskPriority = document.getElementById("new-task-priority");
+  const newTaskPriority = document.getElementById("new-task-priority");
 
   //ul where new tasks will live on the DOM
   const newTaskUl = document.getElementById("tasks");
